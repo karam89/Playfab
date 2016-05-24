241110fe6ef314eb1a4eade0751a038e77c4e460
Karam Bwidani <en.karam1989@gmail.com>
2016-05-24 17:36:15 +0300

    First Version

---
+++ test2.js
@@ -0,0 +1,31 @@
+handlers.sendFriendRequested = function (args)
+{
+	var messageGroupId = args.toId + "_friendRequests2";
+	var playerData = server.GetUserReadOnlyData({"PlayFabId" : args.toId, "Keys" : ["hasFriendsRequestsSharedGroup"]});
+	return {groupId: messageGroupId, date: playerData.Data};
+	var dataPayload = {};
+	var keyString = currentPlayerId;
+	dataPayload[keyString] = "nice request";
+	server.CreateSharedGroupData({
+		SharedGroupId: messageGroupId
+    });
+	return {groupId: messageGroupId, keyString: keyString};
+}
+handlers.helloWorld = function (args)
+{
+	var playerData = server.GetUserReadOnlyData({"PlayFabId" : currentPlayerId, "Keys" : ["SaveState"]});
+	var previousState = 0; //if we return a matching key-value pair, then we can proceed otherwise we will need to create a new record.
+	log.info("will it log?");
+	if(playerData.Data.hasOwnProperty("SaveState"))
+	{
+		previousState = parseInt(playerData.Data["SaveState"].Value);
+	}
+	var toSave = previousState+parseInt(args.toAdd);
+	var result = server.UpdateUserReadOnlyData({"PlayFabId" : currentPlayerId, "Data" : {SaveState: toSave}, "Permission":"Public" });
+
+
+//	info.log("or not?");
+	
+	var message = "Hello " + args.name + "!";
+	return { messageValue: message, CPID:  currentPlayerId, name: args.name, a: previousState, z: toSave};
+}
